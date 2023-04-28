b6264c2aac9fdfc376a452231a7c56f5df929b13
Sagar R.B <sagarrb1805@gmail.com>
2023-04-28 10:17:15 +0530

    Palindrome Check

---
+++ Sample/Program.cs
@@ -0,0 +1,37 @@
+﻿class Palindrome
+{
+    private string reverseString(string s)
+    {
+        char[] charsArrayString = s.ToCharArray();
+        Array.Reverse(charsArrayString);
+        String revString = new String(charsArrayString);
+        return revString;
+    }
+    public bool checkPalindrome(string str)
+    {
+        Console.WriteLine(reverseString(str));
+        if (str == reverseString(str)) {
+            return true;
+        }
+        return false;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Palindrome palindrome = new Palindrome();
+        Console.WriteLine("Enter the string to check for palindrome: ");
+        string palindromeCheckString = Console.ReadLine();
+        if (palindrome.checkPalindrome(palindromeCheckString))
+        {
+            Console.WriteLine("{0} is palindrome.", palindromeCheckString);
+        }
+        else
+        {
+            Console.WriteLine("{0} is not palindrome.", palindromeCheckString);
+        }
+    }
+   
+
+}
